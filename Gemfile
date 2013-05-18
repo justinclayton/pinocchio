b2730a568cf6068664c014b7adee4423363d0c6e
Justin Clayton <justin@claytons.net>
2013-05-18 14:48:15 -0700

    first working commit!

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'cucumber'
 gem 'vagrant-prison'
+gem 'vagrant-sync'
 gem 'puppetlabs_spec_helper'
 gem 'redis'
 
--- lib/pinocchio/api/vagrant.rb
+++ lib/pinocchio/api/vagrant.rb
@@ -1,9 +1,9 @@
 # pinocchio/api/vagrant
 module Pinocchio
   def self.assign_test_manifest test_manifest
-    p source = test_manifest
-    p dest   = File.join(Pinocchio.manifests_path, Pinocchio.manifest_file)
-    FileUtils.copy_file(source, dest)
+    source_dir = test_manifest
+    dest_dir   = File.join(Pinocchio.manifests_path, Pinocchio.manifest_file)
+    FileUtils.copy_file(source_dir, dest_dir)
   end
 
   def self.vagrant_init
@@ -23,7 +23,7 @@ module Pinocchio
           ## XXX: box hash or class to manage urls?
           c.vm.box_url = File.expand_path("~/vagrant/boxes/#{box_name}.box")
           c.vm.provision :puppet do |puppet|
-            puppet.module_path    = Pinocchio.module_path
+            puppet.module_path    = 'modules'
             puppet.manifests_path = Pinocchio.manifests_path
             puppet.manifest_file  = Pinocchio.manifest_file
           end
@@ -34,14 +34,32 @@ module Pinocchio
   end
 
   def self.vagrant_provision box_name
-     Vagrant::Command::Provision.new([box_name], @vagrant.construct(:ui_class => Vagrant::UI::Basic)).execute
+    Vagrant::Command::Provision.new([box_name], @vagrant.construct(:ui_class => Vagrant::UI::Basic)).execute
   end
 
   def self.vagrant_up box_name
     # even though we dont care yet, vagrant still needs puppet.manifest_file to exist on vagrant up
-    Pinocchio.manifests_path
-    Pinocchio.manifest_file
     FileUtils.touch File.join(Pinocchio.manifests_path, Pinocchio.manifest_file)
+    FileUtils.mkdir_p Pinocchio.vagrant_prison_module_dir
     Vagrant::Command::Up.new([box_name, '--no-provision'], @vagrant.construct(:ui_class => Vagrant::UI::Basic)).execute
+    # p @vagrant.construct.config.cwd
+    Pinocchio.sync_module_fixtures
+  end
+
+  def self.vagrant_prison_module_dir
+    vagrant_prison_basedir = @vagrant.construct.cwd.to_s
+    return File.join(vagrant_prison_basedir, 'modules')
+  end
+
+  def self.sync_module_fixtures
+    ## since spec/fixtures/modules is full of symlinks, we can't just use
+    ## it as vagrant's modulepath for puppet because they'd all be broken.
+    ## instead, we copy what we need into the vagrant prison's dir.
+    source_files = Dir.glob(File.join(Pinocchio.module_path, '*'))
+    ## XXX: assumes constant of relative path 'modules' in vagrant config; support others
+
+    # module_path = @vagrant.construct.config.global.vm.provisioners.first.config.module_path.to_s
+    p dest_dir = vagrant_prison_module_dir
+    FileUtils.cp_r(source_files, dest_dir)
   end
 end
