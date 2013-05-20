46f32bba23ce6053fbc7acc0371fe332e41bc2d0
Justin Clayton <justin@claytons.net>
2013-05-20 15:40:03 -0700

    comment cleanup

--- examples/redis/features/support/env.rb
+++ examples/redis/features/support/env.rb
@@ -1,12 +1,4 @@
 require 'cucumber/rspec/doubles'
-require 'rake'
-# require 'pinocchio'
 require 'pinocchio/cucumber'
 
-# Pinocchio.boxes = ['centos6' => 'https://s3.amazonaws.com/justinclayton-pinocchio/boxes/centos6.box']
 Pinocchio.exposed_ports = ['6379']
-
-
-#Before do
-#  Pinocchio.before
-#end
